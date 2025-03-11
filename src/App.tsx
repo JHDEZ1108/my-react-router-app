49bfe5d0545043361b11caaf5e13e356ddf5a303
Josué Hernández <joheca1998@outlook.com>
2025-03-11 12:25:26 -0600

    Implementing nested routes and lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import { Routes, Route, Link } from "react-router-dom";
 import Product from "./components/Products/Product";
 import Home from "./components/Home/Home";
+import Dashboard from "./components/Dashboards/Dashboard";
+import { Suspense } from "react";
+import Settings from "./components/Settings/Settings";
+import Overview from "./components/Overview/Overview";
 
 function App() {
 
@@ -15,10 +19,7 @@ function App() {
             <Link to="/about">About</Link>
           </li>
           <li>
-            <Link to="/product/1">Product 1</Link>
-          </li>
-          <li>
-            <Link to="/product/2">Product 2</Link>
+            <Link to="/dashboard">Dashboard</Link>
           </li>
         </ul>
       </nav>
@@ -27,6 +28,15 @@ function App() {
         <Route path="/" element={<Home />}></Route>
         <Route path="/about" element={<About />}></Route>
         <Route path="/product/:id" element={<Product />}></Route>
+        <Route path="/dashboard" element={<Dashboard />}>
+          <Route path="overview" element={<Overview />}></Route>
+          <Route path="settings" element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <Settings />
+            </Suspense>
+          }>
+          </Route>
+        </Route>
       </Routes>
       
     </div>
