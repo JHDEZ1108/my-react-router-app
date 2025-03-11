01c614fdd989aa19ba2c8063e949ca80d068f38b
Josué Hernández <joheca1998@outlook.com>
2025-03-11 11:44:12 -0600

    Implementing dynamic routing and programmatic navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,36 @@
 import { Routes, Route, Link } from "react-router-dom";
-
+import Product from "./components/Products/Product";
+import Home from "./components/Home/Home";
 
 function App() {
 
   return (
     <div>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-          <Link to="/about">About</Link>
-        </li>
-      </ul>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/product/1">Product 1</Link>
+          </li>
+          <li>
+            <Link to="/product/2">Product 2</Link>
+          </li>
+        </ul>
+      </nav>
       
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/about" element={<About />}></Route>
+        <Route path="/product/:id" element={<Product />}></Route>
       </Routes>
       
     </div>
   )
-}
-
-function Home() {
-  return <h2>Home</h2>
 }
 
 function About() {
